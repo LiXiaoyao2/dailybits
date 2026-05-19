e5223f1d544a93f79abb4800efe7adec30683668
renwei <1340451024@qq.com>
2026-05-19 10:42:03 +0800

    set push time limit to eight

--- src/lib/subscriptions/push-times.test.ts
+++ src/lib/subscriptions/push-times.test.ts
@@ -12,6 +12,10 @@ test("push time updates can keep historical subscriptions above the current limi
     "10:00",
     "11:00",
     "12:00",
+    "13:00",
+    "14:00",
+    "15:00",
+    "16:00",
   ];
 
   assert.equal(getAllowedPushTimeCountForUpdate(existingTimes), existingTimes.length);
@@ -19,21 +23,42 @@ test("push time updates can keep historical subscriptions above the current limi
 });
 
 test("push time updates cannot add more times to historical subscriptions over the limit", () => {
-  const existingTimes = ["08:00", "09:00", "10:00"];
+  const existingTimes = [
+    "08:00",
+    "09:00",
+    "10:00",
+    "11:00",
+    "12:00",
+    "13:00",
+    "14:00",
+    "15:00",
+    "16:00",
+  ];
 
   assert.equal(
     isPushTimeCountAllowedForUpdate(
-      [...existingTimes, "11:00"],
+      [...existingTimes, "17:00"],
       existingTimes,
     ),
     false,
   );
 });
 
-test("push time updates enforce the current limit for normal subscriptions", () => {
-  assert.equal(isPushTimeCountAllowedForUpdate(["09:30", "14:00"], ["09:30"]), true);
+test("push time updates enforce the eight-time limit for normal subscriptions", () => {
+  const eightTimes = [
+    "08:00",
+    "09:00",
+    "10:00",
+    "11:00",
+    "12:00",
+    "13:00",
+    "14:00",
+    "15:00",
+  ];
+
+  assert.equal(isPushTimeCountAllowedForUpdate(eightTimes, ["09:30"]), true);
   assert.equal(
-    isPushTimeCountAllowedForUpdate(["09:30", "14:00", "17:00"], ["09:30"]),
+    isPushTimeCountAllowedForUpdate([...eightTimes, "16:00"], ["09:30"]),
     false,
   );
 });
